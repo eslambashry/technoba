142053d3cfe30bcf07e38a1542c6fb8b04cf0c26
Eslam <eslamhussin600@gmail.com>
2025-11-26 06:03:31 +0200

    merge with main

--- src/DB/models/caseStudyModel.js
+++ src/DB/models/caseStudyModel.js
@@ -8,13 +8,15 @@ const caseStudySchema = new Schema({
     institute_en: { type: String, required: true },
     description_ar: { type: String, required: true },
     description_en: { type: String, required: true },
-    stats: [
-            { value: "10x", label: "Traffic Capacity" },
-            { value: "99.99%", label: "Uptime" },
-            { value: "40%", label: "Cost Savings" },
-        ],
-        status: { type: String, required: true },
-        customId: { type: String},
+    status: [
+    {
+        value_ar: { type: String, required: true },
+        value_en: { type: String, required: true },
+        label_ar: { type: String, required: true },
+        label_en: { type: String, required: true }
+    }
+    ],
+    customId: { type: String},
     image: {
             imageLink:{type: String, required: true},
             public_id:{type: String, required: true},
